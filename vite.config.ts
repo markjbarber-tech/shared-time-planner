007036efc5093dfda9b1cf275308db1cce8f663b
markjbarber-tech <mark.j.barber@gmail.com>
2026-04-14 14:45:32 +1000

    Set base path for shared time planner

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/shared-time-planner/'
   define: {
     __BUILD_TIMESTAMP__: JSON.stringify(new Date().toISOString()),
   },
